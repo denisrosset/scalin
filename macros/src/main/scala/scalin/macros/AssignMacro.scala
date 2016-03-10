f81fd4d1677299fda8c933e793c7aaf70f7d93d3
Denis Rosset <physics@denisrosset.com>
2016-03-10 14:35:43 +0800

    Scala 2.10 macro compatibility.

--- macros/src/main/scala/scalin/macros/AssignMacro.scala
+++ macros/src/main/scala/scalin/macros/AssignMacro.scala
@@ -1,7 +1,7 @@
 package scalin
 package macros
 
-import spire.macros.compat.{termName, freshTermName, resetLocalAttrs, Context, setOrig}
+import spire.macros.compat.{termName, Context}
 
 object AssignMacro {
 
@@ -10,15 +10,15 @@ object AssignMacro {
   def updateOp[B](c: Context)(rhs: c.Expr[B]): c.Expr[Unit] = {
     import c.universe._
     val Pattern = """(.*)\$colon\$eq""".r
-    def transform(tn: TermName): TermName = tn match {
-      case TermName(Pattern(op)) => TermName("set" + operatorNames(op).capitalize)
+    def transform(tn: TermName): TermName = tn.toString match {
+      case Pattern(op) => termName(c)("set" + operatorNames(op).capitalize)
     }
     c.macroApplication match {
-      case q"$_.assignOps[$_]($lhs.apply(...$args)).$method[$_]($_)" =>
+      case q"$x1.assignOps[$x2]($lhs.apply(...$args)).$method[$x3]($x4)" =>
         c.Expr[Unit](q"$lhs.${transform(method)}(..${args.head}, $rhs)")
-      case q"$_.assignOps[$_]($lhs.apply[$_](...$args)).$method[$_]($_)" =>
+      case q"$x1.assignOps[$x2]($lhs.apply[$x3](...$args)).$method[$x4]($x5)" =>
         c.Expr[Unit](q"$lhs.${transform(method)}(..${args.head}, $rhs)")
-      case q"$_.assignOps[$_]($lhs).$method[$_]($_)" =>
+      case q"$x1.assignOps[$x2]($lhs).$method[$x3]($x4)" =>
         c.Expr[Unit](q"$lhs.${transform(method)}($rhs)")
       case _ =>
         sys.error(s"Not implemented for tree ${showRaw(c.macroApplication)} ${show(c.macroApplication)}")
