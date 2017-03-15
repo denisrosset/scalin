8348740812426c5116508e4a3d2cae2c3f62c67c
Denis Rosset <physics@denisrosset.com>
2017-03-15 21:29:32 +0800

    Mat syntax tests.

--- core/src/test/scala/scalin/MatSyntaxTests.scala
+++ core/src/test/scala/scalin/MatSyntaxTests.scala
@@ -1,99 +1,82 @@
 package scalin
-package algebra
 
 import spire.math.Rational
 
-import scalin.immutable.Mat
+class MatSyntaxTests extends ScalinSuite {
 
-class MatEngineTests extends ScalinSuite {
-
-  implicit val engine: MatField[Rational, Mat[Rational]] = immutable.dense.matField[Rational]
-  implicit val engineB: MatEngine[Boolean, Mat[Boolean]] = immutable.dense.matEngine[Boolean]
-  implicit val vecEngine: VecEngine[Rational, Vec[Rational]] = immutable.dense.vecEngine[Rational]
+  import immutable.dense._
 
   test("tabulate") {
-    engine.tabulate(2, 2)( (i, j) => Rational(i+j)) shouldBe rowMajor[Rational](2, 2)(0, 1, 1, 2)
-  }
-
-  test("fill") {
-    engine.fill(2, 2)(Rational(1)) shouldBe rowMajor[Rational](2, 2)(1, 1, 1, 1)
-  }
-
-  test("fillConstant") {
-    engine.fillConstant(2, 2)(Rational(1)) shouldBe rowMajor[Rational](2, 2)(1, 1, 1, 1)
+    tabulate(2, 2)( (i, j) => Rational(i+j)) shouldBe rowMajor[Rational](2, 2)(0, 1, 1, 2)
   }
 
   test("colMajor") {
-    engine.colMajor(2, 2)(1, 2, 3, 4) shouldBe rowMajor[Rational](2, 2)(1, 3, 2, 4)
+    colMajor(2, 2)(1, 2, 3, 4) shouldBe rowMajor[Rational](2, 2)(1, 3, 2, 4)
   }
 
   test("rowMajor") {
-    engine.rowMajor(2, 2)(1, 2, 3, 4) shouldBe colMajor[Rational](2, 2)(1, 3, 2, 4)
+    rowMajor(2, 2)(1, 2, 3, 4) shouldBe colMajor[Rational](2, 2)(1, 3, 2, 4)
   }
 
   test("rowMat") {
-    engine.rowMat(1, 2, 3) shouldBe rowMajor[Rational](1, 3)(1, 2, 3)
+    rowMat(1, 2, 3) shouldBe rowMajor[Rational](1, 3)(1, 2, 3)
   }
 
   test("colMat") {
-    engine.colMat(1, 2, 3) shouldBe rowMajor[Rational](3, 1)(1, 2, 3)
+    colMat(1, 2, 3) shouldBe rowMajor[Rational](3, 1)(1, 2, 3)
   }
 
   test("toRowMat") {
-    engine.toRowMat(vec[Rational](1, 2, 3)) shouldBe rowMajor[Rational](1, 3)(1, 2, 3)
+    vec[Rational](1, 2, 3).toRowMat shouldBe rowMajor[Rational](1, 3)(1, 2, 3)
   }
 
   test("toColMat") {
-    engine.toColMat(vec[Rational](1, 2, 3)) shouldBe colMajor[Rational](3, 1)(1, 2, 3)
-  }
-
-  test("fromMat") {
-    engine.fromMat(rowMajor[Rational](2,2)(1,2,3,4)) shouldBe rowMajor[Rational](2,2)(1,2,3,4)
+    vec[Rational](1, 2, 3).toColMat shouldBe colMajor[Rational](3, 1)(1, 2, 3)
   }
 
   test("count") {
-    engine.count(rowMajor[Rational](2,2)(1,2,3,4))(_ > 2) shouldBe 2
+    rowMajor[Rational](2,2)(1,2,3,4).count(_ > 2) shouldBe 2
   }
 
   test("flatten") {
     val a: Mat[Mat[Rational]] = immutable.DenseMat.tabulate(2, 2)((i, j) => rowMajor[Rational](1, 1)(i+j))
-    engine.flatten(a) shouldBe rowMajor[Rational](2, 2)(0, 1, 1, 2)
+    a.flatten shouldBe rowMajor[Rational](2, 2)(0, 1, 1, 2)
   }
 
   test("flatMap") {
     val a = rowMajor[Rational](2,2)(0,1,1,2)
-    engine.flatMap(a)(x => rowMajor[Rational](1, 1)(x + 1)) shouldBe rowMajor[Rational](2, 2)(1, 2, 2, 3)
+    a.flatMap(x => rowMajor[Rational](1, 1)(x + 1)) shouldBe rowMajor[Rational](2, 2)(1, 2, 2, 3)
   }
 
   test("fold") {
     val a = rowMajor[Rational](2, 2)(1, 2, 3, 4)
-    engine.fold(a)(Rational.one)(_ * _) shouldBe Rational(24)
+    a.fold(Rational.one)(_ * _) shouldBe Rational(24)
   }
 
   test("map") {
     val a = rowMajor[Rational](2, 2)(0, 1, 2, 3)
-    engine.map(a)(_ * 2) shouldBe rowMajor[Rational](2, 2)(0, 2, 4, 6)
+    a.map(_ * 2) shouldBe rowMajor[Rational](2, 2)(0, 2, 4, 6)
   }
 
   test("horzcat") {
     val a = rowMajor[Rational](2, 2)(0, 1, 2, 3)
     val b = rowMajor[Rational](2, 2)(4, 5, 6, 7)
     val res = rowMajor[Rational](2, 4)(0, 1, 4, 5, 2, 3, 6, 7)
-    engine.horzcat(a, b) shouldBe res
+    (a horzcat b) shouldBe res
   }
 
   test("vertcat") {
     val a = colMajor[Rational](2, 2)(0, 1, 2, 3)
     val b = colMajor[Rational](2, 2)(4, 5, 6, 7)
     val res = colMajor[Rational](4, 2)(0, 1, 4, 5, 2, 3, 6, 7)
-    engine.vertcat(a, b) shouldBe res
+    (a vertcat b) shouldBe res
   }
 
   test("slice") {
     val a = rowMajor[Rational](3, 3)(0, 1, 2,
       3, 4, 5,
       6, 7, 8)
-    engine.slice(a, 1 to 2, 1 to 2) shouldBe rowMajor[Rational](2, 2)(4, 5, 7, 8)
+    a(1 to 2, 1 to 2) shouldBe rowMajor[Rational](2, 2)(4, 5, 7, 8)
   }
 
   test("t") {
@@ -103,67 +86,68 @@ class MatEngineTests extends ScalinSuite {
     val res = colMajor[Rational](3, 3)(0, 1, 2,
       3, 4, 5,
       6, 7, 8)
-    engine.t(a) shouldBe res
+    a.t shouldBe res
   }
 
   test("reshape") {
     val a = vec[Rational](1, 2, 3, 4)
     val res = colMajor[Rational](2, 2)(1, 2, 3, 4)
-    engine.reshape(a, 2, 2) shouldBe res
+    a.reshape(2, 2) shouldBe res
   }
 
   test("pointwiseEqual (scalar)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val res = rowMat[Boolean](false, false, true, true)
-    engineB.pointwiseEqual(a, Rational(2)) shouldBe res
+    (a.pointwise == Rational(2)) shouldBe res
   }
 
   test("pointwiseEqual (vec)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val b = rowMat[Rational](0, 2, 1, 2)
     val res = rowMat[Boolean](true, false, false, true)
-    engineB.pointwiseEqual(a, b) shouldBe res
+    (a.pointwise == b) shouldBe res
   }
 
   test("pointwiseNotEqual (scalar)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val res = rowMat[Boolean](true, true, false, false)
-    engineB.pointwiseNotEqual(a, Rational(2)) shouldBe res
+    (a.pointwise != Rational(2)) shouldBe res
   }
 
   test("pointwiseNotEqual (vec)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val b = rowMat[Rational](0, 2, 1, 2)
     val res = rowMat[Boolean](false, true, true, false)
-    engineB.pointwiseNotEqual(a, b) shouldBe res
+    (a.pointwise != b) shouldBe res
   }
 
   test("pointwiseEqv (scalar)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val res = rowMat[Boolean](false, false, true, true)
-    engineB.pointwiseEqv(a, Rational(2)) shouldBe res
+    (a.pointwise === Rational(2)) shouldBe res
   }
 
   test("pointwiseEqv (vec)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val b = rowMat[Rational](0, 2, 1, 2)
     val res = rowMat[Boolean](true, false, false, true)
-    engineB.pointwiseEqv(a, b) shouldBe res
+    (a.pointwise === b) shouldBe res
   }
 
   test("pointwiseNotEqv (scalar)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val res = rowMat[Boolean](true, true, false, false)
-    engineB.pointwiseNeqv(a, Rational(2)) shouldBe res
+    (a.pointwise =!= Rational(2)) shouldBe res
   }
 
   test("pointwiseNotEqv (vec)") {
     val a = rowMat[Rational](0, 1, 2, 2)
     val b = rowMat[Rational](0, 2, 1, 2)
     val res = rowMat[Boolean](false, true, true, false)
-    engineB.pointwiseNeqv(a, b) shouldBe res
+    (a.pointwise =!= b) shouldBe res
   }
 
+  /* TODO
   test("eqv") {
     val a = rowMat[Rational](0, 1, 2)
     val b = rowMat[Rational](0, 1, 2)
@@ -171,95 +155,95 @@ class MatEngineTests extends ScalinSuite {
     engine.eqv(a, b) shouldBe true
     engine.eqv(a, c) shouldBe false
   }
+   */
 
   test("ones") {
-    engine.ones(2, 2) shouldBe rowMajor[Rational](2, 2)(1, 1, 1, 1)
+    ones[Rational](2, 2) shouldBe rowMajor[Rational](2, 2)(1, 1, 1, 1)
   }
 
   test("product") {
-    engine.product(rowMajor[Rational](2, 2)(1,2,3,4)) shouldBe Rational(24)
+    rowMajor[Rational](2, 2)(1,2,3,4).product shouldBe Rational(24)
   }
 
   test("times") {
     val a = rowMat[Rational](1, 2, 3, 4)
-    engine.times(a, Rational(2)) shouldBe rowMat[Rational](2, 4, 6, 8)
-    engine.times(Rational(2), a) shouldBe rowMat[Rational](2, 4, 6, 8)
+    (a * Rational(2)) shouldBe rowMat[Rational](2, 4, 6, 8)
+    (Rational(2) *: a) shouldBe rowMat[Rational](2, 4, 6, 8)
     // TODO: test with a noncommutative ring
   }
 
   test("pointwiseTimes") {
     val a = rowMat[Rational](1, 2, 3, 4)
-    engine.pointwiseTimes(a, a) shouldBe rowMat[Rational](1, 4, 9, 16)
+    (a.pointwise * a) shouldBe rowMat[Rational](1, 4, 9, 16)
   }
-
   test("dyad") {
     val a = vec[Rational](1, 2)
     val res = rowMajor[Rational](2, 2)(1, 2, 2, 4)
-    engine.dyad(a, a) shouldBe res
+    (a dyad a) shouldBe res
   }
 
   test("kron") {
     val a = colMat[Rational](1, 2)
-    engine.kron(a, a) shouldBe colMat[Rational](1, 2, 2, 4)
+    (a kron a) shouldBe colMat[Rational](1, 2, 2, 4)
   }
 
   test("zeros") {
-    engine.zeros(2, 2) shouldBe rowMajor[Rational](2, 2)(0, 0, 0, 0)
+    zeros[Rational](2, 2) shouldBe rowMajor[Rational](2, 2)(0, 0, 0, 0)
   }
 
   test("eye") {
-    engine.eye(2) shouldBe rowMajor[Rational](2, 2)(1, 0, 0, 1)
+    eye[Rational](2) shouldBe rowMajor[Rational](2, 2)(1, 0, 0, 1)
   }
 
   test("toDiagMat") {
-    engine.toDiagMat(vec[Rational](1, 2)) shouldBe rowMajor[Rational](2, 2)(1, 0, 0, 2)
+    vec[Rational](1, 2).toDiagMat shouldBe rowMajor[Rational](2, 2)(1, 0, 0, 2)
   }
 
   test("plus") {
     val a = colMat[Rational](0, 1, 2)
     val b = colMat[Rational](2, 2, 2)
     val res = colMat[Rational](2, 3, 4)
-    engine.plus(a, b) shouldBe res
+    (a + b) shouldBe res
   }
 
   test("minus") {
     val a = colMat[Rational](2, 2, 2)
     val b = colMat[Rational](0, 1, 2)
     val res = colMat[Rational](2, 1, 0)
-    engine.minus(a, b) shouldBe res
+    (a - b) shouldBe res
   }
 
   test("negate") {
     val a = colMat[Rational](2, -1, 0)
     val res = colMat[Rational](-2, 1, 0)
-    engine.negate(a) shouldBe res
+    (-a) shouldBe res
   }
 
   test("pointwisePlus") {
     val a = colMat[Rational](1, 2, 3)
     val res = colMat[Rational](2, 3, 4)
-    engine.pointwisePlus(a, Rational(1)) shouldBe res
+    (a.pointwise + Rational(1)) shouldBe res
   }
 
   test("pointwiseMinus") {
     val a = colMat[Rational](2, 3, 4)
     val res = colMat[Rational](1, 2, 3)
-    engine.pointwiseMinus(a, Rational(1)) shouldBe res
+    (a.pointwise - Rational(1)) shouldBe res
   }
 
   test("nnz") {
     val a = rowMajor[Rational](2, 2)(0, 1, 0, 1)
-    engine.nnz(a) shouldBe 2
+    a.nnz shouldBe 2
   }
 
   test("sum") {
     val a = rowMajor[Rational](2, 2)(1, 2, 3, 4)
-    engine.sum(a) shouldBe Rational(10)
+    a.sum shouldBe Rational(10)
   }
 
   test("trace") {
     val a = rowMajor[Rational](2, 2)(1, 2, 3, 4)
-    engine.trace(a) shouldBe Rational(5)
+    a.trace shouldBe Rational(5)
   }
 
   test("times (mat, mat)") {
@@ -271,7 +255,7 @@ class MatEngineTests extends ScalinSuite {
       4, 5, 12)
     val res = rowMajor[Rational](2, 3)(50, 94, 178,
       60, 120, 220)
-    engine.times(a, b) shouldBe res
+    (a * b) shouldBe res
   }
 
   test("frobenius product") {
@@ -279,15 +263,15 @@ class MatEngineTests extends ScalinSuite {
       1, -1, 2)
     val b = rowMajor[Rational](2, 3)(8, -3, 2,
       4, 1, -5)
-    engine.frobenius(a, b) shouldBe Rational(21)
+    (a frobenius b) shouldBe Rational(21)
   }
 
   test("determinant") {
     // Wikipedia example: determinant of 3x3 matrix
-    engine.determinant(rowMajor[Rational](3,3)(
+    rowMajor[Rational](3,3)(
       -2, 2,-3,
       -1, 1, 3,
-      2, 0,-1)) shouldBe Rational(18)
+      2, 0,-1).determinant shouldBe Rational(18)
   }
 
   test("rank") {
@@ -296,41 +280,46 @@ class MatEngineTests extends ScalinSuite {
       2,7,3,9,
       1,5,3,1,
       1,2,0,8)
-    engine.rank(a) shouldBe 3
+    a.rank shouldBe 3
   }
 
   test("gcd") {
     val a = rowMat[Rational](2, 4, -2)
-    engine.gcd(a) shouldBe Rational(2)
+    a.gcd shouldBe Rational(2)
   }
 
   test("lcm") {
     val a = rowMat[Rational](2, 3, 6)
-    engine.lcm(a) shouldBe Rational(6)
+    a.lcm shouldBe Rational(6)
   }
 
   test("orthogonalized") {
     val a = eye[Rational](2)
-    engine.orthogonalized(a) shouldBe a
+    a.orthogonalized shouldBe a
   }
+/*
+    def pointwiseDiv(lhs: Mat[A], rhs: Mat[A]): MA
+
+  def div(lhs: Mat[A], rhs: A): MA
+ */
 
   test("inverse") {
     val a = rowMajor[Rational](2, 2)(1, 0, 0, 2)
     val res = rowMajor[Rational](2, 2)(Rational.one, Rational.zero, Rational.zero, Rational(1, 2))
-    engine.inverse(a) shouldBe res
+    a.inverse shouldBe res
   }
 
   test("pointwiseDiv") {
     val a = rowMat[Rational](2, 4, 4, 2)
     val b = rowMat[Rational](2, 1, 4, 1)
     val res = rowMat[Rational](1, 4, 1, 2)
-    engine.pointwiseDiv(a, b) shouldBe res
+    (a.pointwise / b) shouldBe res
   }
 
   test("div") {
     val a = rowMat[Rational](2, 4, 4, 6)
     val res = rowMat[Rational](1, 2, 2, 3)
-    engine.div(a, Rational(2)) shouldBe res
+    (a / Rational(2)) shouldBe res
   }
 
 }
